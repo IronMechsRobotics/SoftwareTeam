741fc75b35f604e3f55cdd71fa1b8ad034db8f71
IronMechsRobotics <ironmechs@trentoncatholic.org>
2018-02-01 14:45:06 -0500

    update Drive by Joystick.
    Drive by joystick will eventually overwrite drivebytwojoysticks.

--- src/org/usfirst/frc/team5684/robot/IO.java
+++ src/org/usfirst/frc/team5684/robot/IO.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class IO {
 
-	Joystick leftStick = new Joystick(0);
-	Joystick rightStick = new Joystick(1);
+	public final Joystick leftStick = new Joystick(0);
+	public final Joystick rightStick = new Joystick(1);
 	Button rightFire = new JoystickButton(rightStick, 1);
 	Button leftFire = new JoystickButton(leftStick, 1);
 	Button radTest = new JoystickButton(rightStick,11);
---
+++ src/org/usfirst/frc/team5684/robot/commands/DrivebyJoystick.java
@@ -0,0 +1,78 @@
+package org.usfirst.frc.team5684.robot.commands;
+
+import org.usfirst.frc.team5684.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DrivebyJoystick extends Command {
+	private static double DEAD_BAND = 0.1;
+	private static double ROTATE_DEAD_BAND= 0.1;
+
+	public DrivebyJoystick() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		this.requires(Robot.drivetrain);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double move = Robot.io.leftStick.getY();
+		double rotate = Robot.io.rightStick.getX();
+		boolean turbo = Robot.io.rightStick.getRawButton(1) || Robot.io.leftStick.getRawButton(1);
+		if (Math.abs(move) < DEAD_BAND) {
+			move = 0;
+		} else {
+			if (turbo) {
+				move = map(move, 0, 1.0);
+			} else {
+				move = map(move, 0, 0.5);
+			}
+		}
+		if (Math.abs(rotate) < ROTATE_DEAD_BAND) {
+			rotate = 0;
+		} else {
+			if (turbo) {
+				rotate = map(rotate, 0, 0.6);
+			} else {
+				rotate = map(rotate, 0, 0.3);
+			}
+		}
+
+		Robot.drivetrain.drive(move, rotate);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+
+	private static double map(double x, double out_min, double out_max) {
+		return map(x, DEAD_BAND, 1.0, out_min, out_max);
+	}
+
+	private static double map(double x, double in_min, double in_max, double out_min, double out_max) {
+		boolean negative = x < 0;
+		double newValue = (Math.abs(x) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+		if (negative) {
+			return -newValue;
+		} else {
+			return newValue;
+		}
+	}
+}
